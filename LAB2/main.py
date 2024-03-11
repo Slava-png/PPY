ed54d0899e68963d507a5ae7067605ae0cc856ad
Sviatoslav Pylyp <pylyp.sviatoslav@gmail.com>
2024-03-11 17:58:32 +0100

    Task 4

--- LAB2/main.py
+++ LAB2/main.py
@@ -75,10 +75,13 @@ Extend the previous Python program to include tuples. Now, in addition to creati
 input_numbers = input("Enter a series of space-separated integers: ")
 
 # Convert Input:
-
+input_list = input_numbers.split()
+numbers_list = [int(num) for num in input_list]
+numbers_tuple = tuple(int(num) for num in input_list)
 
 # Sorting:
-
+numbers_list.sort();
+sorted_tuple = tuple(sorted(numbers_tuple))
 
 # Print Output
 print("Sorted list:", numbers_list)
